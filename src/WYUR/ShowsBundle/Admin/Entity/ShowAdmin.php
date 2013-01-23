97b77554124e79aa7629177d7199accc7611a61b
Yair Silbermintz <Yair@Silbermintz.com>
2013-01-22 22:24:15 -0500

    hide slot on show page for people without ROLE_WYUR_SHOW_ADMIN_CREATE (closes #6)

--- src/WYUR/ShowsBundle/Admin/Entity/ShowAdmin.php
+++ src/WYUR/ShowsBundle/Admin/Entity/ShowAdmin.php
@@ -9,6 +9,17 @@ use Sonata\AdminBundle\Show\ShowMapper;
 
 class ShowAdmin extends Admin
 {
+    /**
+     * @var \Symfony\Component\Security\Core\SecurityContext
+     */
+    protected $security_context;
+
+    public function setSecurityContext($security_context)
+    {
+        $this->security_context = $security_context;
+    }
+
+
     public function configureShowFields(ShowMapper $showMapper)
     {
         $showMapper
@@ -29,9 +40,15 @@ class ShowAdmin extends Admin
             ->add('hosts')
             ->add('description', null, array(), array('help' => 'This is the main show description block. <a href="http://www.bbcode.org/reference.php">BBCode</a> can be used to add rich content.'))
             ->add('soundCloudID', null, array(), array('name' => 'SoundCloud ID', 'help' => 'This is the ID number for the shows soundcloud account'))
-            ->add('slot')
-            ->add('imageFile', 'file', array('required' => false, 'help' => 'Images must be png, jpg, or gif & less than 1MB.<br />Images are forced to be square, 105px on the grid page and 260px on the show page.'))
         ;
+
+        /** @var $user \Application\Sonata\UserBundle\Entity\User */
+        $user = $this->security_context->getToken()->getUser();
+        if ($user->hasRole('ROLE_WYUR_SHOW_ADMIN_CREATE')) {
+			$formMapper->add('slot');
+		}
+		
+		$formMapper->add('imageFile', 'file', array('required' => false, 'help' => 'Images must be png, jpg, or gif & less than 1MB.<br />Images are forced to be square, 105px on the grid page and 260px on the show page.'));
     }
 
     public function configureListFields(ListMapper $listMapper)
