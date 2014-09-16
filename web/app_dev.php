0559379486305a76235af2d1fe48af8c16e4a58f
Yair Silbermintz <Yair@Silbermintz.com>
2014-09-15 23:02:33 -0400

    Updated app_dev with latest IP

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,9 +12,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
-        '96.242.217.217',
-        '67.151.45.162',
-        '::1',
+        '96.234.74.180',
     ))
 ) {
     header('HTTP/1.0 403 Forbidden');
